283f3358b8c223a3ef32a1c71f32cbd15f6881a5
JAKOB LIDBORN <jala12@student.bth.se>
2023-06-27 12:45:45 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -153,7 +153,7 @@ async function getMember() {
         spreadsheetId: '1B9fSq_IKIfa5-ABKtGunFh6tiqpTbGVYiWtX1pimafc',
         range: 'A1:B1'
     }
-    sheets.spreadsheets.values.get(request, function(err, response) {
+    gapi.sheets.spreadsheets.values.get(request, function(err, response) {
         var test = String(response.data.values).split(',');
         console.log(test);
     })
